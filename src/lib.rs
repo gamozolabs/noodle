066039da6c1a5dc7acb7894df8a8526f1982d0d7
Brandon Falk <bfalk@gamozolabs.com>
2019-10-14 16:33:57 -0700

    Seems to work, weird codegen issue

---
+++ src/lib.rs
@@ -0,0 +1,220 @@
+use core::convert::TryInto;
+
+trait Serialize {
+    fn serialize(&self, buf: &mut Vec<u8>);
+}
+
+trait Deserialize: Sized {
+    fn deserialize(ptr: &mut &[u8]) -> Option<Self>;
+}
+
+/// Implement `Serialize` trait for types which provide `to_le_bytes()`
+macro_rules! serialize_le {
+    // Serialize `$input_type` as an `$wire_type` by using `to_le_bytes()`
+    // and `from_le_bytes()`. The `$input_type` gets converted to an
+    // `$wire_type` via `TryInto`
+    ($input_type:ty, $wire_type:ty) => {
+        impl Serialize for $input_type {
+            fn serialize(&self, buf: &mut Vec<u8>) {
+                let wire: $wire_type = (*self).try_into()
+                    .expect("Should never happen, input type to wire type");
+                buf.extend_from_slice(&wire.to_le_bytes());
+            }
+        }
+
+        impl Deserialize for $input_type {
+            fn deserialize(orig_ptr: &mut &[u8]) -> Option<Self> {
+                // Get the slice pointed to by `orig_ptr`
+                let ptr: &[u8] = *orig_ptr;
+
+                // Convert the slice to a fixed-size array 
+                let arr: [u8; std::mem::size_of::<$wire_type>()] =
+                    ptr.get(0..std::mem::size_of::<$wire_type>())?
+                        .try_into().ok()?;
+
+                // Convert the array of bytes into the `$wire_type`
+                let wire_val = <$wire_type>::from_le_bytes(arr);
+
+                // Try to convert the wire-format type into the desired type
+                let converted: $input_type = wire_val.try_into().ok()?;
+
+                // Update the pointer
+                *orig_ptr = &ptr[std::mem::size_of::<$wire_type>()..];
+
+                // Return out the deserialized `Self`!
+                Some(converted)
+            }
+        }
+    };
+
+    // Serialize an $input_type using `to_le_bytes()` and `from_le_bytes()`
+    ($input_type:ty) => {
+        serialize_le!($input_type, $input_type);
+    };
+}
+
+serialize_le!(u8);
+serialize_le!(u16);
+serialize_le!(u32);
+serialize_le!(u64);
+serialize_le!(u128);
+serialize_le!(i8);
+serialize_le!(i16);
+serialize_le!(i32);
+serialize_le!(i64);
+serialize_le!(i128);
+serialize_le!(usize, u64);
+serialize_le!(isize, i64);
+
+/*
+/// Implement serialize for `&str`
+impl Serialize for &str {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        // Serialize the underlying bytes of the string
+        Serialize::serialize(self.as_bytes(), buf);
+    }
+}
+
+/// Implement serialize for `[T]`
+impl<T: Serialize> Serialize for [T] {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        // Serialize the number of elements
+        Serialize::serialize(&self.len(), buf);
+
+        // Serialize all of the values
+        self.iter().for_each(|x| Serialize::serialize(x, buf));
+    }
+}
+
+/// Implement serialize for Vec<T>
+impl<T: Serialize> Serialize for Vec<T> {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        // Serialize the number of elements
+        Serialize::serialize(&self.len(), buf);
+
+        // Serialize all of the values
+        self.iter().for_each(|x| Serialize::serialize(x, buf));
+    }
+}
+
+/// Implement `Serialize` trait for arrays of types which implement `Serialize`
+macro_rules! serialize_arr {
+    ($arrsize:expr) => {
+        impl<T: Serialize> Serialize for [T; $arrsize] {
+            fn serialize(&self, buf: &mut Vec<u8>) {
+                // Serialize all of the values
+                self.iter().for_each(|x| Serialize::serialize(x, buf));
+            }
+        }
+    }
+}
+
+serialize_arr!(0);
+serialize_arr!(1);
+serialize_arr!(2);
+serialize_arr!(3);
+serialize_arr!(4);
+serialize_arr!(5);
+serialize_arr!(6);
+serialize_arr!(7);
+serialize_arr!(8);
+serialize_arr!(9);
+serialize_arr!(10);
+serialize_arr!(11);
+serialize_arr!(12);
+serialize_arr!(13);
+serialize_arr!(14);
+serialize_arr!(15);
+serialize_arr!(16);
+serialize_arr!(17);
+serialize_arr!(18);
+serialize_arr!(19);
+serialize_arr!(20);
+serialize_arr!(21);
+serialize_arr!(22);
+serialize_arr!(23);
+serialize_arr!(24);
+serialize_arr!(25);
+serialize_arr!(26);
+serialize_arr!(27);
+serialize_arr!(28);
+serialize_arr!(29);
+serialize_arr!(30);
+serialize_arr!(31);
+serialize_arr!(32);*/
+
+macro_rules! ser {
+    (
+        struct $structname:ident {
+            $($id:ident: $typ:ty),*$(,)?
+        }
+    ) => {
+        #[derive(Debug)]
+        struct $structname {
+            $(
+                $id: $typ,
+            )*
+        }
+        
+        impl Serialize for $structname {
+            fn serialize(&self, buf: &mut Vec<u8>) {
+                $(
+                    Serialize::serialize(&self.$id, buf);
+                )*
+            }
+        }
+
+        impl Deserialize for $structname {
+            fn deserialize(orig_ptr: &mut &[u8]) -> Option<Self> {
+                // Get the original pointer
+                let mut ptr = *orig_ptr;
+
+                // Construct this structure field-by-field
+                let ret = $structname {
+                    $($id: Deserialize::deserialize(&mut ptr)?),*
+                };
+
+                // Update the original pointer
+                *orig_ptr = ptr;
+
+                // Return out the result
+                Some(ret)
+            }
+        }
+    }
+}
+
+ser!(struct Baz {
+    foo: u32,
+});
+
+impl Drop for Baz {
+    fn drop(&mut self) {
+        print!("DROPPIN STUFFS\n");
+    }
+}
+
+ser!(struct Foo {
+    foo: u32,
+    bar: u8,
+    baz: Baz,
+    bat: u16,
+});
+
+#[test]
+fn test() {
+    let nested_struct = Foo {
+        foo: 0xdeadbeef,
+        bar: 0x32,
+        baz: Baz { foo: 0x12345678 },
+        bat: 0x9999,
+    };
+
+    let mut buf = Vec::new();
+    nested_struct.serialize(&mut buf);
+    print!("Serialized {:#x?}\n", buf);
+
+    let mut ptr = &buf[..buf.len()-0];
+    print!("Deserialized {:#x?}\n", Foo::deserialize(&mut ptr));
+}
+
